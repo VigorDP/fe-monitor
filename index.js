93a4f262c9f2d82a4faaf18ba3845cf10fc50ad8
fengyinchao <15926339107@163.com>
2019-08-02 19:02:08 +0800

    feat: init

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ var express = require('express'),
   cookieParser = require('cookie-parser');
 var app = express();
 
-//设置端口
-var port = process.env.PORT || 3000;
-app.set('port', port);
-
 //解析cookie
 app.use(cookieParser());
 
 require('./routes/backend')(app);
 
-app.listen(3001, () => console.log('listen 3001'))
+app.listen(process.env.PORT || 3000, () => console.log('listen 3001'))
